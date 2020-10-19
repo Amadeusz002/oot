fc028e7f8ed9690d99d3dd7e87d33c7f8d56ef6a
PKopel <pawel.kopel2@gmail.com>
2020-10-19 19:46:52 +0200

    complete first task (StudentDAO)

--- lab-persistence/orm/src/main/java/pl/edu/agh/iisg/to/dao/StudentDao.java
+++ lab-persistence/orm/src/main/java/pl/edu/agh/iisg/to/dao/StudentDao.java
@@ -4,18 +4,36 @@ import java.util.Collections;
 import java.util.Map;
 import java.util.Optional;
 
+import org.hibernate.Session;
 import pl.edu.agh.iisg.to.model.Course;
 import pl.edu.agh.iisg.to.model.Student;
+
+import javax.persistence.PersistenceException;
+import javax.persistence.Query;
 
 public class StudentDao extends GenericDao<Student> {
 
     public Optional<Student> create(final String firstName, final String lastName, final int indexNumber) {
-    	//TODO - implement
+    	Student student = new Student(firstName,lastName,indexNumber);
+    	try{
+    	    this.save(student);
+    	    return Optional.of(student);
+        } catch (PersistenceException e){
+    	    e.printStackTrace();
+        }
         return Optional.empty();
     }
 
     public Optional<Student> findByIndexNumber(final int indexNumber) {
-    	//TODO - implement
+    	final Session session = currentSession();
+        try{
+            Student student = session.createQuery("SELECT  s FROM Student s WHERE s.indexNumber = :index", Student.class)
+                    .setParameter("index", indexNumber)
+                    .getSingleResult();
+            return Optional.ofNullable(student);
+        } catch (PersistenceException e){
+            e.printStackTrace();
+        }
         return Optional.empty();
     }
 
--- lab-persistence/orm/src/main/java/pl/edu/agh/iisg/to/model/Student.java
+++ lab-persistence/orm/src/main/java/pl/edu/agh/iisg/to/model/Student.java
@@ -30,7 +30,7 @@ public class Student {
     @ManyToMany(mappedBy = "studentSet")
     private Set<Course> courseSet = new HashSet<>();
 
-    Student() {
+    protected Student() {
     }
 
     public Student(final String firstName, final String lastName, final int indexNumber) {
